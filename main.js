776e9d592416924d2033b7f0b1d7bcf177fabd95
RexCorpDev <rexcorpfx@gmail.com>
2016-06-13 17:19:53 -0700

    city, state, title works. NEXT = detail render

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
 'use strict';
 $(document).ready(init);
 const API_KEY = 'e4edd15ee58cd59f';
+let settingsObj = {
+  qtype     :   '',
+  city      :   '',
+  qcity     :   '',
+  state     :   ''
+};
 
 function init(){
+  $('body').click(event => {
+    $('.panel-heading').data('qtype', event.target.className);
+  });
+
   $('.search-settings').submit('click', webCamInit);
-  $('.search-state').on('click', setState);
+  $('.search-state').on('click', getState);
 };
 
 function webCamInit(event){
   event.preventDefault(); // stops the form from making a request using a query string. e.g. 'localhost:3000/?'
-  let settingsObj = {
-    city : $('.search-input-city').val()
-  };
+  settingsObj.city = getCity(),
+  settingsObj.state = $('.panel-heading').data('state')
+  $('.panel-heading').data('qtype').match(/webcam-list/) ? settingsObj.qtype = 'Weather Cams' : settingsObj.qtype = '';
   getWebCams(settingsObj);
 };
 
 function getWebCams(optObj){
   $('.data-body').empty();
-  let city;
-  !optObj.city ? city = 'autoip' : city = optObj.city.split(' ').join('_');
-
 
   // LATITUDE & LONGITUDE
   // navigator.geolocation.getCurrentPosition((pos)=> {
   //   console.log('lat: ', pos.coords.latitude, 'long: ', pos.coords.longitude);
   // });
 
-
-
   // AJAX REQUEST
-  $.getJSON(`http://api.wunderground.com/api/${API_KEY}/webcams/q/${city}.json`)
+  $.getJSON(`http://api.wunderground.com/api/${API_KEY}/webcams/q/${optObj.state}/${optObj.city}.json`)
   .done(data => {
     console.log(data);
-    if(!data.webcams) return $('div.film-data-body').append(data.response.error.type);
+    if(!data.webcams.length) return $('div.data-body').addClass('error').append(data.response.error.description);
     let cards = renderCams(data);
     let $camList = $('<div>').addClass('row col-xs-8 col-xs-offset-2 text-center').append(cards);
+    $('.weather-data-title').text(`${$('.search-input-city').val()}, ${optObj.state} - ${optObj.qtype}`);
     $('.data-body').append($camList);
   })
   .error(()=> {
@@ -55,7 +61,16 @@ function renderCams(data){
   return cards;
 };
 
-function setState(){
+function renderTitle(city, state, query){
+  $('.weather-data-title').text(`${city} - ${state} : ${query}`);
+}
+function getState(){
   let state = $(this).children().text();
-  $('.webcam-btn-get').data('state', state);
+  $('.panel-heading').data('state', state);
+  $('.weather-input-choice').addClass('text-primary state-chosen').text(state);
+};
+function getCity(){
+  let city = $('.search-input-city').val()
+  !city ? city = 'autoip' : city = city.split(' ').join('_');
+  return city;
 };
